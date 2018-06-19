d7715bc61666270e322e37d5541120f28b9f4ea4
Manohar R Mankala <manohar.mankala@gmail.com>
2018-06-19 07:30:17 -0400

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,31 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    db.collection('Todos').insertOne({
+        text: 'todo 1',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: 'Manu',
+        age: 32,
+        location: 'Columbus, OH'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
+    db.close();
+});
